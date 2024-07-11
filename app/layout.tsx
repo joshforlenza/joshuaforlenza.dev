39c2353842d8b915cd216ba2f0f2cc1026fd7947
joshforlenza <joshforlenza@gmail.com>
2024-07-11 14:25:57 -0400

    added navbar, intro, and about me sections

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
+import Header from "@/components/header";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,7 +18,7 @@ export default function RootLayout({
     <html lang="en">
       <body
         className={`${inter.className}
-      bg-gray-50 text-gray-950 relative`}
+      bg-gray-50 text-gray-950 relative h-[5000px] pt-28 sm:pt-36`}
       >
         <div
           className="bg-[#fbe2e3] absolute top-[-6rem] -z-10 right-[11rem] 
@@ -28,6 +29,7 @@ export default function RootLayout({
         h-[31.25rem] w-[50rem] rounded-full blur-[10rem] 
         sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]"
         ></div>
+        <Header />
         {children}
       </body>
     </html>
