5e8c926e179ae780dfbca47237c04c05e38722de
joshforlenza <joshforlenza@gmail.com>
2024-07-13 18:36:12 -0400

    Added theme toggle

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import Header from "@/components/header";
 import ActiveSectionContextProvider from "@/context/active-section-context";
 import { Toaster } from "react-hot-toast";
 import Footer from "@/components/footer";
+import ThemeSwitch from "@/components/theme-switch";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -38,6 +39,7 @@ export default function RootLayout({
           <Footer />
           <Toaster position="top-right" />
         </ActiveSectionContextProvider>
+        <ThemeSwitch />
       </body>
     </html>
   );
